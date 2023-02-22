b6b14154943085399488ed28d7d5c9a1080ae90a
frav1401 <91700092+frav1401@users.noreply.github.com>
2023-02-22 15:54:51 -0500

    Ajout du code pour openCR

---
+++ Code/OpenCR/CodeJumpy/mainJumpy/mainJumpy.ino
@@ -0,0 +1,11 @@
+#include "ControleDynamixel.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  setupControleDynamixel();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mainControleDynamixel();
+}
