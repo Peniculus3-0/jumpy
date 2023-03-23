83daf237b212dc9a1b593150cdfc39c4734af9b6
christophelajoie <lajc1503@usherbrooke.ca>
2023-03-22 21:07:02 -0400

    Animation progress

--- UI/animation.py
+++ UI/animation.py
@@ -1,55 +1,63 @@
-import tkinter
+import tkinter as tk
+import math
 import time
 
-Window_Width = 800
+root = tk.Tk()
 
-Window_Height = 600
+canvas = tk.Canvas(root, width=500, height=500)
+canvas.pack()
 
-Ball_Start_XPosition = 50
+############# Initial conditions ################
 
-Ball_Start_YPosition = 50
+Foot_Xorigin = 250
+Foot_Yorigin = 500
 
-Ball_Radius = 30
+Foot_length = 50
+Bottom_length = 150
+Top_length = 150
+r = 50
 
-Ball_min_movement = 5
+Foot = canvas.create_line(Foot_Xorigin, Foot_Yorigin, Foot_Xorigin + Foot_length, Foot_Yorigin)
+Bottom = canvas.create_line(Foot_Xorigin + 35, Foot_Yorigin, Foot_Xorigin + 30, Foot_Yorigin - Bottom_length)
+Top = canvas.create_line(Foot_Xorigin + 30, Foot_Yorigin - Bottom_length, Foot_Xorigin + 50, Foot_Yorigin - 350)
+Head = canvas.create_oval(300 - r,150 - r,300 + r,150 + r)
 
-Refresh_Sec = 0.01
+def rotate(goal_angle, line):
+    rad = math.radians(goal_angle)
+    x1, y1, x2, y2 = canvas.coords(line)
+    dx = x2 - x1
+    dy = y2 - y1
+    new_dx = dx * math.cos(rad) - dy * math.sin(rad)
+    new_dy = dx * math.sin(rad) + dy * math.cos(rad)
+    new_x2 = x1 + new_dx
+    new_y2 = y1 + new_dy
 
+    canvas.coords(line, x1, y1, new_x2, new_y2)
 
-def create_animation_window():
-    Window = tkinter.Tk()
-    Window.title("Animation")
+def update(line, line2):
+    x1, y1, x2, y2 = canvas.coords(line)
+    x3, y3, x4, y4 = canvas.coords(line2)
 
-    Window.geometry(f'{Window_Width}x{Window_Height}')
-    return Window
+    if canvas.type(line2) == 'oval':
+        canvas.coords(line2, x2 - r, y2 - r, x2 + r, y2 + r)
+    else:
+        #canvas.coords(line2, x2, y2, x2 + (cos()*Top_length), y2 + (cos()*Top_length)) Figure out how to prevent Top leg part from stretching
+        canvas.coords(line2, x2, y2, x4, y4)
 
+def animate(angle):
+    rotate(angle, Bottom)
+    rotate(-angle,Top)
+    update(Bottom, Top)
+    update(Top, Head)
 
-def create_animation_canvas(Window):
-    canvas = tkinter.Canvas(Window)
-    canvas.configure(bg="Blue")
-    canvas.pack(fill="both", expand=True)
-    return canvas
+angle_range = 35.0
+angle_speed = -0.5
 
+i = 1
+for theta in range(abs(int(angle_range/angle_speed))):
+    canvas.update()
+    canvas.after(30,animate(angle_speed))
+    i = i + 1
 
-def animate_ball(Window, canvas, xinc, yinc):
-    ball = canvas.create_oval(Ball_Start_XPosition - Ball_Radius,
-                              Ball_Start_YPosition - Ball_Radius,
-                              Ball_Start_XPosition + Ball_Radius,
-                              Ball_Start_YPosition + Ball_Radius,
-                              fill="Red", outline="Black", width=4)
-    while True:
-        canvas.move(ball, xinc, yinc)
-        Window.update()
-        time.sleep(Refresh_Sec)
-        ball_pos = canvas.coords(ball)
-        # unpack array to variables
-        al, bl, ar, br = ball_pos
-        if al < abs(xinc) or ar > Window_Width - abs(xinc):
-            xinc = -xinc
-        if bl < abs(yinc) or br > Window_Height - abs(yinc):
-            yinc = -yinc
+root.mainloop()
 
-
-Animation_Window = create_animation_window()
-Animation_canvas = create_animation_canvas(Animation_Window)
-animate_ball(Animation_Window, Animation_canvas, Ball_min_movement, Ball_min_movement)
