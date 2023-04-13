7462c82fb758734a2aa1f049eb4c417d027fdbd6
Antoine <mara1205@usherbrooke.com>
2023-04-13 14:08:54 -0400

    Clean the code

--- UI/BluetoothUI.py
+++ UI/BluetoothUI.py
@@ -1,11 +1,86 @@
 from bleak import BleakScanner, BleakClient, discover
 import asyncio
 import logging
-import UI
 import threading
 device_address = "94:A9:A8:3A:52:90"
-CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_U = "0000ffe1-0000-1000-8000-00805f9b34fb"
+import UI
 
+
+combo_box_values = []
+
+# class BLEScanner: 
+#     def __init__(self, device_list):
+#         self.device_list = device_list
+        
+#     async def scan_devices(self):
+#         devices = await discover()
+#         self.device_list.extend(devices)
+
+# async def start_scanning(device_list):
+#     print("start scanning")
+#     scanner = BLEScanner(device_list)
+#     await scanner.scan_devices()
+#     print("scan finished-----")
+    
+def thread_scan_devices(app, device_list):
+    current_thread = threading.current_thread()
+    print("Current Thread: {}".format(current_thread.name))
+    mythread = threading.Thread(target=scan_devices, args=(device_list,))
+    mythread.start()
+    mythread.join()
+    print("thread finished")
+    print(device_list)
+    current_thread = threading.current_thread()
+    print("Current Thread: {}".format(current_thread.name))
+    on_scan_finished(app,device_list)
+
+def on_scan_finished(app,device_list):
+        current_thread = threading.current_thread()
+        print("Current Thread: {}".format(current_thread.name))
+        print("Scan finished.")
+        for dev in device_list:
+            print("Device %s, name=%s " % (dev.address, dev.name))
+            # app.textbox.insert(tk.END, "Device %s, Name=%s \n" % (dev.address, dev.name))
+            if dev.name != None:
+                combo_box_values.append(dev.address +  str(dev.name))
+
+        if combo_box_values != []:
+        
+            app.label_avertissement.configure(text="Select a device")
+            app.combobox.configure(state="readonly")
+            app.combobox.configure(values=(combo_box_values))
+            # app.button_scan.configure(state="disabled")
+            # app.button_connect.configure(state="normal")
+        else:
+            app.label_avertissement.configure(text="No device found")
+            # app.button_scan.configure(state="normal")
+            # app.button_connect.configure(state="disabled")
+            app.combobox.configure(state="disabled")
+            app.combobox.configure(values=[])
+            app.combobox.set("")
+
+    
+
+    
+def scan_devices(device_list):
+    current_thread = threading.current_thread()
+    print("Current Thread: {}".format(current_thread.name))
+    asyncio.run(start_scanning(device_list))
+    
+async def start_scanning(device_list):
+
+        devices = await BleakScanner.discover()
+        device_list.extend(devices)
+
+        
+
+           
+        
+
+def connect_to_device_async(device_address):
+    print(threading.current_thread().name)
+    asyncio.run(connect_to_device(device_address))
 
 async def connect_to_device(message):
     logging.info(f'Current registered tasks: {len(asyncio.all_tasks())}')
@@ -13,8 +88,8 @@ async def connect_to_device(message):
     async with BleakClient(device_address) as client:
         # do something with the client connection
         if client.is_connected:
-            UI.App.label_avertissement.configure(text="Connected")
-            await client.start_notify(CHARACTERISTIC_UUID, callback)
+            app.label_avertissement.configure(text="Connected")
+            await client.start_notify(CHARACTERISTIC_U, callback)
             while open :
                 await asyncio.sleep(1)
 
@@ -23,62 +98,8 @@ async def connect_to_device(message):
             # print("Services:")
             # for service in services:
             #     print(service)
-                await client.write_gatt_char(CHARACTERISTIC_UUID, bytearray(message.encode()))
+                await client.write_gatt_char(CHARACTERISTIC_U, bytearray(message.encode()))
                     
         else:
             app.label_avertissement.configure(text="Not Connected")
 
-#             class BLEScanner: 
-# def __init__(self, device_list):
-#         self.device_list = device_list
-        
-async def scan_devices(self):
-        devices = await discover()
-        self.device_list.extend(devices)
-
-async def start_scanning(device_list):
-    print("start scanning")
-    scanner = BLEScanner(device_list)
-    await scanner.scan_devices()
-    print("scan finished-----")
-    
-def on_scan_finished(device_list):
-    print("Scan finished.")
-    for dev in device_list:
-        print("Device %s, name=%s " % (dev.address, dev.name))
-        # app.textbox.insert(tk.END, "Device %s, Name=%s \n" % (dev.address, dev.name))
-        if dev.name != None:
-            combo_box_values.append(dev.address + " " + str(dev.name))
-
-    if combo_box_values != []:
-        app.label_avertissement.configure(text="Select a device")
-        app.combobox.configure(state="readonly")
-        app.combobox.configure(values=(combo_box_values))
-        # app.button_scan.configure(state="disabled")
-        # app.button_connect.configure(state="normal")
-    else:
-        app.label_avertissement.configure(text="No device found")
-        # app.button_scan.configure(state="normal")
-        # app.button_connect.configure(state="disabled")
-        app.combobox.configure(state="disabled")
-        app.combobox.configure(values=[])
-        app.combobox.set("")
-    
-async def readdevice():
-    async with BleakClient(device_address) as client:
-        x = await client.read_gatt_char(CHARACTERISTIC_UUID)
-        print(bytes(x).decode("utf-8"))
-        return bytes(x).decode("utf-8")
-
-        
-
-def start_scanning_async():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_scanning(device_list))
-    loop.close()
-    on_scan_finished(device_list)
-
-def connect_to_device_async(device_address):
-    print(threading.current_thread().name)
-    asyncio.run(connect_to_device(device_address))
--- UI/UI.py
+++ UI/UI.py
@@ -13,15 +13,19 @@ import time
 import logging
 import BluetoothUI
 
+
+
 logging.basicConfig(level=logging.DEBUG, filename='UI.log',format='%(asctime)s:%(levelname)s:%(message)s')
 
 device_address = "94:A9:A8:3A:52:90"
 CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
 message = 's'
 stop = 'e'
-label_text = " "
+label_text = "Bienvenue "
 device_list = []
 combo_box_values = []
+
+
 
 
 
@@ -30,41 +34,64 @@ def setlabel():
     print(thread.name)
 
 
+
+class MotorPanel(customtkinter.CTkFrame):
+    def __init__(self, *args, header_name="Motor Panel", **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.header_name = header_name
+
+        self.header = customtkinter.CTkLabel(self, text=self.header_name, font=("Helvetica", 16, "bold"))
+        self.header.grid(row=0, column=0, padx=10, pady=10)
+
+
+        self.speed_label = customtkinter.CTkLabel(self, text="Motor Speed:", )
+        self.speed_label.grid(row=1, column=0, padx=10, pady=10)
+
+        self.torque_label =customtkinter.CTkLabel(self, text="Motor Torque:")
+        self.torque_label.grid(row=2, column=0, padx=10, pady=10)
+    
+    def set_torque(self, torque):
+        self.torque_label.configure(text= "Motor Speed: "+ str(torque))
+
+    def set_speed(self, speed):
+        self.speed_label.configure(text= "Motor speed:" + str(speed))
+        
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        current_thread = threading.current_thread()
+        print("Current Thread: {}".format(current_thread.name))
+        # load images 
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+        self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "logo-color.png")), size=(100, 50))
+        self.robot_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "robot.png")), size=(250, 250))
+
 
         self.title("Bluetooth Application")
         self.geometry("700x450")
-        button_font = customtkinter.CTkFont(family="Arial", size=30, weight="bold")
+        self.iconbitmap(os.path.join(image_path, "logo.ico"))
+
+        button_font = customtkinter.CTkFont(family="Arial", size=20)	
         label_font = customtkinter.CTkFont(family="Arial", size=20)
+
 
         # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(2, weight=1)
 
        
-
-        # load images 
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-        self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "logo-color.png")), size=(100, 50))
-        self.robot_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "robot.png")), size=(250, 250))
-
-        # set icon
-        self.iconbitmap(os.path.join(image_path, "logo.ico"))
-
-        # c 
+        # create navigation frame
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0, )
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(2, weight=1)
 
-         # create animation frame
 
 
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text=" jumpy",
                                                              compound="left", font=button_font)
         
-    
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
         self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
@@ -73,21 +100,21 @@ class App(customtkinter.CTk):
         self.home_button.grid(row=1, column=0, sticky="ew")
 
 
-
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
                                                                 command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
+
         # create home frame
-        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, bg_color="gray20")
         self.home_frame.grid_columnconfigure(0, weight=1)
         self.home_frame.grid_rowconfigure(2, weight=1)
 
+        self.motor_panel = MotorPanel(self.home_frame)
+        self.motor_panel.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
+        self.motor_panel.set_speed(0)
 
-
-        # self.connect_button = customtkinter.CTkButton(self.home_frame, text="Connect", compound="right", command=lambda: threading.Thread(target=connect_to_device_async, args=(message)).start())
-        # self.connect_button.grid(row=2, column=0, padx=20, pady=10)
-       
+        
 
         self.jump_button = customtkinter.CTkButton(self.home_frame, text="JUMP",font=button_font, compound="bottom", height=60, width=200, 
                                                    command=lambda: threading.Thread(target=BluetoothUI.connect_to_device_async, args=(message)).start()) ## Pour commencer un thread ne pas mettre () après la fonction
@@ -97,82 +124,33 @@ class App(customtkinter.CTk):
         self.jump_button = customtkinter.CTkButton(self.home_frame, text="send",font=button_font, compound="bottom", height=60, width=200, 
                                                    command=lambda: threading.Thread(target=setlabel, args=()).start())
         
-        self.jump_button.grid(row=2, column=0, padx=40, pady=20)
+        self.jump_button.grid(row=0, column=1, padx=40, pady=20)
 
         self.text=customtkinter.CTkTextbox(self.home_frame, height=1, width=100)
         self.text.grid(row=1, column=0, padx=40, pady=20)
-        self.find_button = customtkinter.CTkButton(self.home_frame, text="Find", font=button_font, compound="bottom", height=60, width=200, 
-                                                   command=lambda: threading.Thread(target=start_scanning_async, args=(device_list)).start())
-        self.find_button.grid(row=3, column=0, padx=40, pady=20)
+        self.find_button = customtkinter.CTkButton(self.home_frame, text="Scan", font=button_font, compound="bottom", height=60, width=200, 
+                                                   command=lambda: BluetoothUI.thread_scan_devices(self, device_list)) #mettre une virgule après args sinon ça ne marche pas
+        self.find_button.grid(row=0, column=2, padx=40, pady=20)
      
         self.label_avertissement = customtkinter.CTkLabel(self.home_frame, text=label_text, font=label_font)
-        self.label_avertissement.grid(row=5, column=2, padx=20, pady=20)
+        self.label_avertissement.grid(row=1, column=1, padx=20, pady=20)
 
-        # self.textbox = customtkinter.CTkTextbox(self.home_frame, width=400, corner_radius=0)
-        # self.textbox.grid(row=5, column=0, sticky="n", padx=20, pady=20)
+        self.combobox = customtkinter.CTkComboBox(self.home_frame,  values=["Device"])
+        self.combobox.grid(row=3, column=1, padx=20, pady=20)
 
-        # self.combobox = customtkinter.CTkComboBox(self.home_frame,  values=["Device"])
-        # self.combobox.grid(row=5, column=0, sticky="n", padx=20, pady=20)
-
-        # self.robot_label = customtkinter.CTkLabel(self.home_frame, image=self.robot_image, text=None)
-        # self.robot_label.grid(row=2, column=0)
-
-# if multiple frame
-
-        # create second frame
-        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-
-        # create third frame
-        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-
-        # select default frame
-        # self.select_frame_by_name("home")
-
-    
-        player = animationv4.Animation(self.home_frame)
-        player.grid(row=1, column=0, sticky="nsew")
-
-        # set button color for selected button
-        # self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        # self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        # self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
-
-        # show selected frame
    
         self.home_frame.grid(row=0, column=1, sticky="nsew")
-        # else:
-        #     self.home_frame.grid_forget()
-        # if name == "frame_2":
-        #     self.second_frame.grid(row=0, column=1, sticky="nsew")
-        # else:
-        #     self.second_frame.grid_forget()
-        # if name == "frame_3":
-        #     self.third_frame.grid(row=0, column=1, sticky="nsew")
-        # else:
-        #     self.third_frame.grid_forget()
-
-
-
-    def home_button_event(self):
-        self.select_frame_by_name("home")
-
-    # def frame_2_button_event(self):
-    #     self.select_frame_by_name("frame_2")
-
-    # def frame_3_button_event(self):
-    #     self.select_frame_by_name("frame_3")
+        
 
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
 
-
 if __name__ == "__main__":
-
     app = App()
     app.mainloop()
+    
 
 
-
-
+    
 
